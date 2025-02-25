b54ac6d4fbe903210954a30d8a43bc60e6b19839
Felix <felixcesar2078@gmail.com>
2025-02-25 13:33:55 -0500

    feat: log in y primer dashboard con rutas listo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './features/auth/log-in/log-in.component';
+import { MainComponent } from './features/pages/main/main.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LogInComponent },
+    { path: 'main', component: MainComponent, canActivate: [AuthGuard] },
+    { path: '**', redirectTo: '' }
+  ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/features/auth/log-in/log-in.component.ts
+++ src/app/features/auth/log-in/log-in.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-log-in',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule], 
   templateUrl: './log-in.component.html',
-  styleUrl: './log-in.component.css'
+  styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent {
+  username = '';
+  password = '';
 
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.username === 'admin' && this.password === '1234') {
+      localStorage.setItem('token', 'true');
+      this.router.navigate(['/main']);
+    } else {
+      alert('Credenciales incorrectas');
+    }
+  }
 }
