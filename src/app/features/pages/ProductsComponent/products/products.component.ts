116e7c1ba4f6c9dafbffa94afe950316dc47b334
Felix <felyrics3@gmail.com>
2025-02-28 18:56:35 -0500

    feat: mejoras al estilo de los demas componentes, dejo listo para comenzar a implementar modales, formularios y tablas

--- src/app/features/pages/ProductsComponent/products/products.component.ts
+++ src/app/features/pages/ProductsComponent/products/products.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { ActionPanelComponent } from '../../../../shared/components/action-panel/action-panel.component';
+import { Component, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { AddProductsComponent } from '../add-products/add-products.component';
 
 @Component({
   selector: 'app-products',
-  imports: [ActionPanelComponent],
+  imports: [ButtonComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+
+    private readonly dialog = inject(MatDialog);
+  
+
+    openModal() {
+      const dialogRef = this.dialog.open(AddProductsComponent, {
+        width: '1800px',
+      });
+    }
 
 }
--- src/app/features/pages/ventas/ventas.component.ts
+++ src/app/features/pages/ventas/ventas.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-ventas',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './ventas.component.html',
   styleUrl: './ventas.component.css'
 })
