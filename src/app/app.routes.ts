a21e4848654520a34bd3d29b976e40d78d06e57f
Felix <felyrics3@gmail.com>
2025-02-28 19:20:04 -0500

    feat: mejoras en el ruteo, se elimino main-routing.module.ts ya que era innecesario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LogInComponent } from './features/auth/log-in/log-in.component';
+import { MainComponent } from './features/pages/main/main.component';
+import { DashboardComponent } from './features/pages/dashboard/dashboard.component';
+import { AuthGuard } from './core/guards/auth.guard';
+import { ProductsComponent } from './features/pages/ProductsComponent/products/products.component';
+import { VentasComponent } from './features/pages/ventas/ventas.component';
+import { UsuariosComponent } from './features/pages/UsersComponent/usuarios/usuarios.component';
 
 export const routes: Routes = [
   { path: '', component: LogInComponent, pathMatch: 'full' },
-  { path: 'main', loadChildren: () => import('./features/pages/main/main-routing.module').then(m => m.MainRoutingModule) },
+  { 
+    path: 'main', 
+    component: MainComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'sale', component: VentasComponent },
+      { path: 'users', component: UsuariosComponent },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+    ]
+  },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
