d6608dbb61da99eebaa66fa19d1688577846f67f
Felix <felyrics3@gmail.com>
2025-02-28 19:09:48 -0500

    feat: reduccion de codigo en la logica del sidebar

--- src/app/shared/components/left-sidebar/left-sidebar.component.ts
+++ src/app/shared/components/left-sidebar/left-sidebar.component.ts
@@ -2,40 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component, input, output } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatTooltip } from '@angular/material/tooltip';
-
-
+import { sidebarItems } from './sidebar-items';
 
 @Component({
   selector: 'app-left-sidebar',
-  imports: [RouterModule, CommonModule,MatTooltip],
+  imports: [RouterModule, CommonModule, MatTooltip],
   templateUrl: './left-sidebar.component.html',
   styleUrl: './left-sidebar.component.css',
 })
 export class LeftSidebarComponent {
+  
   isLeftSidebarCollapsed = input.required<boolean>();
   changeIsLeftSidebarCollapsed = output<boolean>();
-  items = [
-    {
-      routeLink: '/main/dashboard',
-      icon: 'fal fa-home',
-      label: 'Inicio',
-    },
-    {
-      routeLink: '/main/products',
-      icon: 'fal fa-hammer',
-      label: 'Productos',
-    },
-    {
-      routeLink: '/main/sale',
-      icon: 'fal fa-shopping-cart',
-      label: 'Ventas',
-    },
-    {
-      routeLink: '/main/users',
-      icon: 'fal fa-users',
-      label: 'Usuarios',
-    }
-  ];
+  items = sidebarItems;
 
   toggleCollapse(): void {
     this.changeIsLeftSidebarCollapsed.emit(!this.isLeftSidebarCollapsed());
---
+++ src/app/shared/components/left-sidebar/sidebar-items.ts
@@ -0,0 +1,22 @@
+export const sidebarItems = [
+    {
+      routeLink: '/main/dashboard',
+      icon: 'fal fa-home',
+      label: 'Inicio',
+    },
+    {
+      routeLink: '/main/products',
+      icon: 'fal fa-hammer',
+      label: 'Productos',
+    },
+    {
+      routeLink: '/main/sale',
+      icon: 'fal fa-shopping-cart',
+      label: 'Ventas',
+    },
+    {
+      routeLink: '/main/users',
+      icon: 'fal fa-users',
+      label: 'Usuarios',
+    }
+  ];
